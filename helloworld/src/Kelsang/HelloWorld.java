0dd5d104716bacdc38e287162af9ce01aeaaedf3
324033521 <324033521@C-JWZ7KS1.tdsb.on.ca>
2017-02-21 11:22:28 -0500

    I am pushing HelloWorld

---
+++ helloworld/src/Kelsang/HelloWorld.java
@@ -0,0 +1,14 @@
+package Kelsang;
+/**
+ * HelloWorld.java
+ * Program prints Hello, World!
+ * FEB 21,2017
+ * @author TenzingKelsang
+ */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
